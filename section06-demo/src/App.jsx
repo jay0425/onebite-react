5310ebc22e1648e245c48ffd0617ca509fd608b5
jay0425 <heedev94@gmail.com>
2024-12-25 17:58:51 +0900

    section06-demo 기능 구현 완료

--- section06-demo/src/App.jsx
+++ section06-demo/src/App.jsx
@@ -1,7 +1,26 @@
 import './App.css';
+import Viewer from './components/Viewer';
+import Controller from './components/Controller';
+import { useState } from 'react';
 
 function App() {
-  return <></>;
+  const [count, setCount] = useState(0);
+
+  const onClickButton = (value) => {
+    setCount(count + value);
+  };
+
+  return (
+    <div className="App">
+      <h1>Counter🌟</h1>
+      <section>
+        <Viewer count={count} />
+      </section>
+      <section>
+        <Controller onClickButton={onClickButton} />
+      </section>
+    </div>
+  );
 }
 
 export default App;
---
+++ section06-demo/src/components/Viewer.jsx
@@ -0,0 +1,10 @@
+const Viewer = ({ count }) => {
+  return (
+    <div>
+      total Number
+      <h1>{count}</h1>
+    </div>
+  );
+};
+
+export default Viewer;
