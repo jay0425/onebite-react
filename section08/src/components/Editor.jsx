00078140c80a48dfec59b386a7e8b22dda2f527d
jay0425 <heedev94@gmail.com>
2024-12-27 21:33:43 +0900

    section08 todolist UI 완성

---
+++ section08/src/components/Editor.jsx
@@ -0,0 +1,11 @@
+import './Editor.css';
+
+const Editor = () => {
+  return (
+    <div className="Editor">
+      <input placeholder="New To do..." />
+      <button>add</button>
+    </div>
+  );
+};
+export default Editor;
