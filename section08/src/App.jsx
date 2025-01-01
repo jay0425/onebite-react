0a759ea357c48329575f9b9311f91e925391255d
jay0425 <heedev94@gmail.com>
2025-01-01 12:34:51 +0900

    content 데이터에 넣기

--- section08/src/App.jsx
+++ section08/src/App.jsx
@@ -2,7 +2,7 @@ import './App.css';
 import Header from './components/Header';
 import Editor from './components/Editor';
 import List from './components/List';
-import { useState } from 'react';
+import { useState, useRef } from 'react';
 
 // 리렌더링 될 때마다 다시 생설될 필요 없고 애초에 상수라 값이 변할 일도 없어서
 // 컴포넌트 바깥에 mockTodos를 두도록 한다.
@@ -29,10 +29,22 @@ const mockTodos = [
 
 function App() {
   const [todos, setTodos] = useState(mockTodos);
+  const idRef = useRef(3);
+
+  const onCreate = (content) => {
+    const newTodo = {
+      id: idRef.current++,
+      isDone: false,
+      content: content,
+      date: new Date().getTime(),
+    };
+
+    setTodos([newTodo, ...todos]);
+  };
   return (
     <div className="App">
       <Header />
-      <Editor />
+      <Editor onCreate={onCreate} />
       <List />
     </div>
   );
--- section08/src/components/Editor.jsx
+++ section08/src/components/Editor.jsx
@@ -1,10 +1,38 @@
+import { useState, useRef } from 'react';
 import './Editor.css';
 
-const Editor = () => {
+const Editor = ({ onCreate }) => {
+  const [content, setContent] = useState('');
+  const contentRef = useRef();
+
+  const onChangeContent = (e) => {
+    setContent(e.target.value);
+  };
+
+  const onKeydown = (e) => {
+    if (e.keyCode === 13) {
+      onSubmit();
+    }
+  };
+
+  const onSubmit = () => {
+    if (content === '') {
+      contentRef.current.focus();
+      return;
+    }
+    onCreate(content);
+    setContent('');
+  };
   return (
     <div className="Editor">
-      <input placeholder="New To do..." />
-      <button>add</button>
+      <input
+        ref={contentRef}
+        value={content}
+        onChange={onChangeContent}
+        onKeyDown={onKeydown}
+        placeholder="New To do..."
+      />
+      <button onClick={onSubmit}>add</button>
     </div>
   );
 };
