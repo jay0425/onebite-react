aa830895f68ad25c0d51772e1f8b33d06e760142
jay0425 <heedev94@gmail.com>
2025-01-02 12:27:45 +0900

    todolist 토글 수정하기 기능 구현

--- section08/src/components/List.jsx
+++ section08/src/components/List.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react';
 import './List.css';
 import TodoItem from './TodoItem';
 
-const List = ({ todos }) => {
+const List = ({ todos, onUpdate }) => {
   const [search, setSearch] = useState('');
 
   const onChangeSearch = (e) => {
@@ -24,7 +24,7 @@ const List = ({ todos }) => {
       <input value={search} onChange={onChangeSearch} placeholder="Please enter your search term" />
       <div className="todos_wrapper">
         {filteredTodos.map((todo) => {
-          return <TodoItem key={todo.id} {...todo} />;
+          return <TodoItem key={todo.id} {...todo} onUpdate={onUpdate} />;
         })}
       </div>
     </div>
