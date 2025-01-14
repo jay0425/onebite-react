9868d29c3123c226eba353d33342018f7d79718d
jay0425 <heedev94@gmail.com>
2025-01-15 08:58:55 +0900

    section12 감정일기 demo 라우터까지 셋팅

---
+++ section12-demo/src/App.jsx
@@ -0,0 +1,21 @@
+import './App.css';
+import { Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import New from './pages/New';
+import Diary from './pages/Diary';
+import Notfound from './pages/Notfound';
+
+function App() {
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/new" element={<New />} />
+        <Route path="/diary" element={<Diary />} />
+        <Route path="*" element={<Notfound />} />
+      </Routes>
+    </>
+  );
+}
+
+export default App;
