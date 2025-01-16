82de92f074a8c09eaadf2ca888579bad629744cd
jay0425 <heedev94@gmail.com>
2025-01-16 21:51:30 +0900

    Button 컴포넌트 생성

--- section12/src/App.jsx
+++ section12/src/App.jsx
@@ -6,6 +6,7 @@ import Diary from './pages/Diary';
 import Notfound from './pages/Notfound';
 
 import { getEmotionImage } from './util/get-emotion-image';
+import Button from './components/Button';
 
 // 1. "/" : 모든 일기를 조회하는 Home 페이지
 // 2. "/new" : 새로운 일기를 작성하는 New 페이지
@@ -20,19 +21,27 @@ function App() {
   };
   return (
     <>
-      <div>
-        <img src={getEmotionImage(1)} alt="이모티콘1" />
-        <img src={getEmotionImage(2)} alt="이모티콘2" />
-        <img src={getEmotionImage(3)} alt="이모티콘3" />
-        <img src={getEmotionImage(4)} alt="이모티콘4" />
-        <img src={getEmotionImage(5)} alt="이모티콘5" />
-      </div>
-      <div>
-        <Link to={'/'}>Home</Link>
-        <Link to={'/new'}>New</Link>
-        <Link to={'/diary'}>Diary</Link>
-      </div>
-      <button onClick={onClickButton}>New 페이지로 이동!!</button>
+      <Button
+        text={'123'}
+        type={'DEFAULT'}
+        onClick={() => {
+          console.log('123번 버튼 클릭!!');
+        }}
+      />
+      <Button
+        text={'123'}
+        type={'POSITIVE'}
+        onClick={() => {
+          console.log('123번 버튼 클릭!!');
+        }}
+      />
+      <Button
+        text={'123'}
+        type={'NEGATIVE'}
+        onClick={() => {
+          console.log('123번 버튼 클릭!!');
+        }}
+      />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/new" element={<New />} />
