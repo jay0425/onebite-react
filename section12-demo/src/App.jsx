6c3fba57e1a1ab6c371703310e4816fb20b69190
jay0425 <heedev94@gmail.com>
2025-01-21 12:50:37 +0900

    전체적인 레이아웃 UI 구현, Edit 페이지와 Diary 페이지 라우팅 useParams 이용해서 구현

--- section12-demo/src/App.jsx
+++ section12-demo/src/App.jsx
@@ -3,23 +3,17 @@ import { Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import New from './pages/New';
 import Diary from './pages/Diary';
+import Edit from './pages/Edit';
 import Notfound from './pages/Notfound';
-import { getEmotionImage } from './util/get-emotion-image';
 
 function App() {
   return (
     <>
-      <div>
-        <img src={getEmotionImage(1)} alt="emotion1" />
-        <img src={getEmotionImage(2)} alt="emotion2" />
-        <img src={getEmotionImage(3)} alt="emotion3" />
-        <img src={getEmotionImage(4)} alt="emotion4" />
-        <img src={getEmotionImage(5)} alt="emotion5" />
-      </div>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/new" element={<New />} />
-        <Route path="/diary" element={<Diary />} />
+        <Route path="/diary/:id" element={<Diary />} />
+        <Route path="/edit/:id" element={<Edit />} />
         <Route path="*" element={<Notfound />} />
       </Routes>
     </>
