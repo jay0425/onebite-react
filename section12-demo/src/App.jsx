1c9f0fef0fded98028d0cc9405da70efcf27cfde
jay0425 <heedev94@gmail.com>
2025-01-23 12:56:28 +0900

    header 구현 완료

--- section12-demo/src/App.jsx
+++ section12-demo/src/App.jsx
@@ -12,7 +12,7 @@ import Header from './components/Header';
 function App() {
   return (
     <>
-      <Header />
+      <Header title={'asdf'} leftChild={'cnm'} rightChild={'wre'} />
       <Button text={'text'} />
       <Routes>
         <Route path="/" element={<Home />} />
