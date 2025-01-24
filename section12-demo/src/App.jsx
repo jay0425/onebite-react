67d55b31634c4a0d0e14f8b38dac86e6bdd8bf2e
jay0425 <heedev94@gmail.com>
2025-01-24 12:48:13 +0900

    일기 추가 기능 구현. 테스트 완료

--- section12-demo/src/App.jsx
+++ section12-demo/src/App.jsx
@@ -1,4 +1,5 @@
 import './App.css';
+import { useReducer, useRef } from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import New from './pages/New';
@@ -6,14 +7,56 @@ import Diary from './pages/Diary';
 import Edit from './pages/Edit';
 import Notfound from './pages/Notfound';
 
-import Button from './components/Button';
-import Header from './components/Header';
+const mockData = [
+  {
+    id: 1,
+    createdDate: new Date().getTime(),
+    emotionId: 1,
+    content: '1번 일기',
+  },
+  {
+    id: 2,
+    createdDate: new Date().getTime(),
+    emotionId: 2,
+    content: '2번 일기',
+  },
+];
+
+function reducer(state, action) {
+  switch (action.type) {
+    case 'CREATE':
+      return [action, ...state];
+  }
+}
 
 function App() {
+  // 일기 내용을 저장할 state
+  const [data, dispatch] = useReducer(reducer, mockData);
+  const idRef = useRef(3);
+
+  // 새로운 일기 추가
+  const onCreate = (createdDate, emotionId, content) => {
+    // 새로운 일기를 추가하는 기능
+    dispatch({
+      type: 'CREATE',
+      data: {
+        id: idRef.current++,
+        createdDate,
+        emotionId,
+        content,
+      },
+    });
+  };
+
   return (
     <>
-      <Header title={'asdf'} leftChild={'cnm'} rightChild={'wre'} />
-      <Button text={'text'} />
+      <button
+        onClick={() => {
+          onCreate(new Date().getTime(), 1, 'Hello');
+        }}
+      >
+        일기 추가 테스트
+      </button>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/new" element={<New />} />
