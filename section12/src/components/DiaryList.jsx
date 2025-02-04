788106b0a9dd0869bfe1f06b50fcdd67f88d13af
jay0425 <heedev94@gmail.com>
2025-02-04 12:45:03 +0900

    DiaryItem UI 구현

--- section12/src/components/DiaryList.jsx
+++ section12/src/components/DiaryList.jsx
@@ -1,5 +1,22 @@
+import './DiaryList';
+import Button from './Button';
+import DiaryItem from './DiaryItem';
+
 const DiaryList = () => {
-  return <div>List</div>;
+  return (
+    <div className="DiaryList">
+      <div className="menu_bar">
+        <select name="" id="">
+          <option value={'latest'}>최신순</option>
+          <option value={'oldest'}>오래된 순</option>
+        </select>
+        <Button text={'새로운 일기 쓰기'} type={'POSITIVE'} />
+      </div>
+      <div className="list_wrapper">
+        <DiaryItem />
+      </div>
+    </div>
+  );
 };
 
 export default DiaryList;
