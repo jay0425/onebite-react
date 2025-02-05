141fb9f68a72b2ea322c085809a3e05a915f4bca
jay0425 <heedev94@gmail.com>
2025-02-05 12:36:59 +0900

    home 페이지 헤더와 DiaryItem에 실제 데이터 넣고 구현하기

--- section12/src/App.jsx
+++ section12/src/App.jsx
@@ -10,15 +10,21 @@ import Notfound from './pages/Notfound';
 const mockData = [
   {
     id: 1,
-    createdDate: new Date().getTime(),
+    createdDate: new Date('2025-02-05').getTime(),
     emotionId: 1,
     content: '1번 일기 내용입니다.',
   },
   {
     id: 2,
-    createdDate: new Date().getTime(),
+    createdDate: new Date('2025-02-04').getTime(),
     emotionId: 2,
     content: '2번 일기 내용입니다.',
+  },
+  {
+    id: 3,
+    createdDate: new Date('2025-01-07').getTime(),
+    emotionId: 3,
+    content: '3번 일기 내용입니다.',
   },
 ];
 
@@ -33,8 +39,8 @@ function reducer(state, action) {
   }
 }
 
-const DiaryStateContext = createContext();
-const DiaryDispatchContext = createContext();
+export const DiaryStateContext = createContext();
+export const DiaryDispatchContext = createContext();
 
 function App() {
   // 일기 내용을 저장할 state
--- section12/src/components/DiaryItem.jsx
+++ section12/src/components/DiaryItem.jsx
@@ -2,8 +2,8 @@ import './DiaryItem.css';
 import { getEmotionImage } from '../util/get-emotion-image';
 import Button from './Button';
 
-const DiaryItem = () => {
-  const emotionId = 5;
+const DiaryItem = ({ id, emotionId, createdDate, content }) => {
+  // const emotionId = 5;
 
   return (
     <div className="DiaryItem">
@@ -11,8 +11,8 @@ const DiaryItem = () => {
         <img src={getEmotionImage(emotionId)} alt="최고 좋은 감정" />
       </div>
       <div className="info_section">
-        <div className="created_date">{new Date().toLocaleDateString()}</div>
-        <div className="content">일기 컨텐츠</div>
+        <div className="created_date">{new Date(createdDate).toLocaleDateString()}</div>
+        <div className="content">{content}</div>
       </div>
       <div className="button_section">
         <Button text={'수정하기'} />
